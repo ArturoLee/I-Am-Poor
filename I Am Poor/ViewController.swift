7abaa541694b1eacf355bd5558d5cc349d5f3152
ArturoLee <arturo9419@gmail.com>
2019-12-09 11:01:29 -0500

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Arturo Lee on 12/9/19.
+//  Copyright © 2019 Arturo Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
